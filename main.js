6123703eea7d044a3fe47e8af2b7d2594e57d954
aishwarya873 <75245093+aishwarya873@users.noreply.github.com>
2021-11-01 20:40:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+img="";
+status = "";
+objects=[];
+function preload(){
+    img=loadImage("dog_cat.jpg");
+
+
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status : Detecting objects";
+
+}
+function draw(){
+image (img,0,0,640,420);
+if(status !=""){
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status : object Detected";
+        fill("#33FFD4");
+        percent= floor(objects[i].confidence*100);
+        text(objects[i].label+"  "+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("#33FFD4");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status = true;
+    objectDetector.detect(img,gotResults);
+    
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        objects=results;
+    }
+}
